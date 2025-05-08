03e3a7b384122184da1f9a633491f3a7a8cf472a
DRS05 <79338608+DRS05@users.noreply.github.com>
2025-05-08 22:30:42 +0200

    Added optional loading from csv

--- main.py
+++ main.py
@@ -14,30 +14,34 @@ if __name__ == '__main__':
     pre_processing = PreProcessing(['datasets\IDMT-SMT-AUDIO-EFFECTS\Gitarre monophon',
                                     'datasets\IDMT-SMT-AUDIO-EFFECTS\Gitarre monophon2'])
     
-    X, y, feature_names, label_names = pre_processing.execute_mean_features()
+    # X, y, feature_names, label_names = pre_processing.execute_mean_features()
 
-    df = pd.DataFrame(X, columns=feature_names)
-    df['label'] = y
-    df.to_csv('guitar_monophon_mean_features.csv', index=False)
+    df = pd.read_csv("guitar_monophon_mean_features.csv")
+
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    # df = pd.DataFrame(X, columns=feature_names)
+    # df['label'] = y
+    # df.to_csv('guitar_monophon_mean_features.csv', index=False)
     X_train_val, X_test, y_train_val, y_test, folds = pre_processing.data_splitting(X, y)
 
     scaler = StandardScaler()
     X_train_val = scaler.fit_transform(X_train_val)
     X_test = scaler.transform(X_test)
 
-    param_grid = {
-        'C': [0.1, 1, 10, 50, 100],
-        'gamma': [0.0001, 0.001, 0.01, 0.1, 1],
-        'kernel': ['rbf']
-    }
+    # param_grid = {
+    #     'C': [0.1, 1, 10, 50, 100],
+    #     'gamma': [0.0001, 0.001, 0.01, 0.1, 1],
+    #     'kernel': ['rbf']
+    # }
 
-    support_vector_machine = SVC()
+    # support_vector_machine = SVC()
 
-    grid = GridSearchCV(support_vector_machine, param_grid, cv=5, scoring='accuracy', verbose=0, n_jobs=-1)
-    grid.fit(X_train_val, y_train_val)
+    # grid = GridSearchCV(support_vector_machine, param_grid, cv=5, scoring='accuracy', verbose=0, n_jobs=-1)
+    # grid.fit(X_train_val, y_train_val)
 
-    print(grid.best_params_)
-    print(grid.best_score_)
+    # print(grid.best_params_)
+    # print(grid.best_score_)
 
     support_vector_machine = SVC(kernel='rbf', C=100, gamma=0.01)
     support_vector_machine.fit(X_train_val, y_train_val)
