c35d98c36c5eaf407eeb0af7f39effbee0fbbf29
DRS05 <79338608+DRS05@users.noreply.github.com>
2025-05-08 22:20:17 +0200

    add preprocessing mean features add baseline svm

--- GuitarFX/data/preprocessing.py
+++ GuitarFX/data/preprocessing.py
@@ -1,64 +1,85 @@
 from typing import List
 import numpy as np
 import librosa
+import os
+import glob
 from sklearn.model_selection import KFold, train_test_split
 
 
 class PreProcessing:
-    def __init__(self, dataset_path: List[str] | str) -> None:
-        self.dataset_path = dataset_path
+    """
+    This class is a pre-processing pipeline is speficically made for the
+    pre-processing of guitar effect classification. However, this can be
+    extended to other audio-related classification tasks.
+    """
+    def __init__(self, dataset_paths: List[str] | str) -> None:
+        """
+        Inputs:
+            dataset_paths (List[str | str]): List of paths or a singular path
+            refering to where the audio dataset(s) are stored. It is presumed
+            that the path to the dataset contains dictonaries that have the
+            classifier as its name and the children of those dictonaries
+            contain the audio files being part of that classification group.
+        """
+        self.dataset_paths = list(dataset_paths)
 
-    def signal_processing(file_path):
+    def signal_processing(self, file_path):
         """
         Pre-process the audio signal.
         """
-        # Get the audio signal
-        y, sr = librosa.load(file_path)
-
-        # Resample at 44.1kHz
-        y_resampled, sr = librosa.resample(y, orig_sr=sr, target_sr=44100)
-
-        # Switch any potential multi channel audio signal to a mono channel
-        y_mono = librosa.to_mono(y_resampled)
+        # Get the audio signal, resample at 44.1kHz and switch any potential multi channel audio signal to a mono channel
+        y, sr = librosa.load(file_path, sr=44100, mono=True)
 
         # Trim leading and trailing silence
-        y_trimmed = librosa.effects.trim(y_mono)
+        y_trimmed, _ = librosa.effects.trim(y)
 
         return y_trimmed, sr
 
-    def extract_mean_features(y, sr):
+    def extract_mean_features(self, y, sr):
         """
         These are the features we use for the competetive baseline model,
         which is support vector machine with radial basis function (RBF)
         kernel. Can also be used for other model that can only one-dimensional
         data.
         """
-        mffcs = librosa.feature.mfcc(y, sr=sr, n_mfcc=20)
-        chromatogram = librosa.feature.chroma_stft(y, sr=sr)
-        spectrogram = librosa.feature.spectral_centroid(y, sr=sr)
-        zero_crossing_rate = librosa.feature.zero_crossing_rate(y, sr=sr)
-        root_mean_squared_error = librosa.feature.rms(y, sr=sr)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
+        chromatogram = librosa.feature.chroma_stft(y=y, sr=sr)
+        spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr)
+        zero_crossing_rate = librosa.feature.zero_crossing_rate(y)
+        room_mean_square = librosa.feature.rms(y=y)
 
-        return {
-            "mffcs": np.mean(mffcs, axis=1),
-            "zero_crossing_rate": np.mean(zero_crossing_rate, axis=1),
-            "chromatogram": np.mean(chromatogram, axis=1),
-            "spectrogram": np.mean(spectrogram, axis=1),
-            "root_mean_squared_error": np.mean(root_mean_squared_error, axis=1),
-        }
+        features = np.concatenate([
+            np.mean(mfcc, axis=1),
+            np.mean(chromatogram, axis=1),
+            np.mean(spectral_contrast, axis=1),
+            np.mean(zero_crossing_rate, axis=1),
+            np.mean(room_mean_square, axis=1)
+        ])
 
-    def extract_signal_representations(y, sr):
+        # feature_names = (
+        #     [f'mfcc_{i+1}' for i in range(20)]
+        #     [f'chroma_{i+1}' for i in range(chromatogram.shape[0])] +
+        #     [f'spectral_contrast_{i+1}' for i in range(spectral_contrast.shape[0])] +
+        #     ['zero_crossing_rate'] +
+        #     ['rms']
+        # )
+
+        # print(feature_names)
+
+        return features
+
+    def extract_signal_representations(self, y, sr):
         """
         Features for the convolutional neural network (CNN)
         """
         pass
 
-    def data_splitting(features, labels):
+    def data_splitting(self, features, labels):
         """
         Split the data in a training split, data split and test split for
         k-fold cross-valiation.
         """
-        X_train, X_test, y_train, y_test = train_test_split(
+        X_train_val, X_test, y_train_val, y_test = train_test_split(
             features,
             labels,
             test_size=0.1,
@@ -67,28 +88,73 @@ class PreProcessing:
         )
 
         kf = KFold(n_splits=5)
-        folds = kf.split(X_train)
+        folds = list(kf.split(X_train_val))
 
-        return X_train, X_test, y_train, y_test, folds
+        return X_train_val, X_test, y_train_val, y_test, folds
 
-    def data_augmentation(data):
+    def data_augmentation(self, data):
         """
         """
         pass
 
-    def data_saving(data):
+    def data_saving(self, dataframe):
         """
         """
         pass
 
-    def data_cleaning(data):
+    def data_cleaning(self, data):
         """
         """
         pass
 
-    def execute():
+    def execute_mean_features(self, max_samples_per_classifier = None):
         """
-        Execute the pre-processing pipeline
+        Extract the mean features for each file in dataset
+
+        Inputs:
+            max_samples_per_classifier (int): Limit the sample amount per
+            classifier to limit the amount of time spent for pre-processing.
         """
-        features = []
-        labels = []
+        feature_names = ['mfcc_1', 'mfcc_2', 'mfcc_3', 'mfcc_4', 'mfcc_5', 'mfcc_6', 'mfcc_7', 'mfcc_8', 'mfcc_9', 'mfcc_10', 'mfcc_11', 'mfcc_12', 'mfcc_13', 'mfcc_14', 'mfcc_15', 'mfcc_16', 'mfcc_17', 'mfcc_18', 'mfcc_19', 'mfcc_20', 'chroma_1', 'chroma_2', 'chroma_3', 'chroma_4', 'chroma_5', 'chroma_6', 'chroma_7', 'chroma_8', 'chroma_9', 'chroma_10', 'chroma_11', 'chroma_12', 'spectral_contrast_1', 'spectral_contrast_2', 'spectral_contrast_3', 'spectral_contrast_4', 'spectral_contrast_5', 'spectral_contrast_6', 'spectral_contrast_7', 'zero_crossing_rate', 'rms']
+        label_names = []
+
+        X = []
+        y = []
+
+        total_files = 20592
+        current_number_file = 0
+
+        for dataset_path in self.dataset_paths:
+            label_dicts = os.listdir(dataset_path)
+            label_names.extend(label_dicts)
+
+            for label_dict in label_dicts:
+                label_dict_path = os.path.join(dataset_path, label_dict)
+
+                for wav_file in os.listdir(label_dict_path):
+                    current_number_file += 1
+                    try:
+                        wav_file_path = os.path.join(label_dict_path, wav_file)
+
+                        signal, sr = self.signal_processing(wav_file_path)
+                        features = self.extract_mean_features(signal, sr)
+
+                        X.append(features)
+                        y.append(label_dict)
+
+                    except Exception as e:
+                        print("Couldn't load file.")
+
+                    print(f"Processed file ({current_number_file}/{total_files})")
+
+                    if max_samples_per_classifier is not None:
+                        if current_number_file % max_samples_per_classifier == 0:
+                            break
+
+        X = np.array(X)
+        y = np.array(y)
+
+        return X, y, feature_names, label_names
+
+
+        
--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-# This is a sample Python script.
-
-def hello_world():
-    return "Hello, World!"
+from GuitarFX.data.preprocessing import PreProcessing
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 
 if __name__ == '__main__':
-    hello_world()
+    """
+    Code to run the baseline model. Will be moved to other part of code later.
+    """
+    pre_processing = PreProcessing(['datasets\IDMT-SMT-AUDIO-EFFECTS\Gitarre monophon',
+                                    'datasets\IDMT-SMT-AUDIO-EFFECTS\Gitarre monophon2'])
+    
+    X, y, feature_names, label_names = pre_processing.execute_mean_features()
+
+    df = pd.DataFrame(X, columns=feature_names)
+    df['label'] = y
+    df.to_csv('guitar_monophon_mean_features.csv', index=False)
+    X_train_val, X_test, y_train_val, y_test, folds = pre_processing.data_splitting(X, y)
+
+    scaler = StandardScaler()
+    X_train_val = scaler.fit_transform(X_train_val)
+    X_test = scaler.transform(X_test)
+
+    param_grid = {
+        'C': [0.1, 1, 10, 50, 100],
+        'gamma': [0.0001, 0.001, 0.01, 0.1, 1],
+        'kernel': ['rbf']
+    }
+
+    support_vector_machine = SVC()
+
+    grid = GridSearchCV(support_vector_machine, param_grid, cv=5, scoring='accuracy', verbose=0, n_jobs=-1)
+    grid.fit(X_train_val, y_train_val)
+
+    print(grid.best_params_)
+    print(grid.best_score_)
+
+    support_vector_machine = SVC(kernel='rbf', C=1.0, gamma='scale')
+    support_vector_machine.fit(X_train_val, y_train_val)
+    y_pred = support_vector_machine.predict(X_test)
+    test_acc = accuracy_score(y_test, y_pred)
+
+    print(f"Test set accuracy: {test_acc:.4f}")
